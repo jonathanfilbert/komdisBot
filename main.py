2ac1c11b249dd1ceb218e6f6a8caad49936746bb
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2019-06-02 22:43:05 +0700

    waiting for twitter auth review

---
+++ main.py
@@ -0,0 +1,41 @@
+import tweepy
+import random
+import time
+
+
+phrasesArray = []
+CONSUMER_KEY = 0
+CONSUMER_SECRET = 0
+ACCESS_TOKEN = 0
+ACCESS_TOKEN_SECRET = 0
+
+
+def main():
+    bot = authenticate()
+    while True:
+        phrase = pilihkata()
+        bot.update_status(phrase.upper())
+        delayEveryHour()
+
+
+def authenticate():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    return api
+
+
+def delayEveryHour():
+    time.sleep(5)
+
+
+def pilihkata():
+    file = open("phrases.txt")
+    content = file.read()
+    file.close()
+    phrasesArray = content.split(",")
+    return random.choice(phrasesArray)
+
+
+if __name__ == "__main__":
+    main()
